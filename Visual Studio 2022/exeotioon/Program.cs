c9f4bf4751889a93e86000c09b5dc97f3cdf1922
mohammad rahimalipour <muhammadi.alipour@gmail.com>
2024-07-10 21:12:14 +0330

    exeption

---
+++ Visual Studio 2022/exeotioon/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exeotioon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int doobl = 0;
+            while (true)
+            {
+                Console.WriteLine("adad khod ra vared konid : ");
+
+                try
+                {
+                    string str = Console.ReadLine();
+                    doobl = (int.Parse(str)) * 2;
+                    Console.WriteLine("do barabar adad : " + doobl + "\n \n");
+
+
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("lotfan meqdar adadi vared konid "+"\n");
+
+                }
+
+
+            }
+        }
+    }
+}
